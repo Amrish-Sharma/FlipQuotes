581836ef523f50ab3c4ba2153f0ed554486c24e8
Amrish Sharma <amrishsharma95@gmail.com>
2025-01-27 11:25:04 +0530

    Feature addsource (#7)
    * added json source for quotes
    
    * changed font of the quote and author
    
    * refresh and share button
    
    * refresh and share button
    
    * added refresh and share button

--- app/src/main/java/com/app/codebuzz/flipquotes/MainActivity.java
+++ app/src/main/java/com/app/codebuzz/flipquotes/MainActivity.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private VerticalViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        VerticalViewPager viewPager = findViewById(R.id.card_view);
+        viewPager = findViewById(R.id.card_view);
         viewPager.setAdapter(new SlidePageAdapter(this));
 
     }
-
+    public VerticalViewPager getViewPager() {
+        return viewPager;
+    }
 
 }
 
---
+++ app/src/main/java/com/app/codebuzz/flipquotes/Quote.java
@@ -0,0 +1,14 @@
+package com.app.codebuzz.flipquotes;
+
+public class Quote {
+    private String quote;
+    private String author;
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
